a99c53285850c4116a071e05a3756e4c6501c1bf
Laura Sans <sans@munatx.com>
2023-05-17 11:27:56 +0200

    Update fastq dir 2 samplesheet for BGI data

--- bin/fastq_dir_to_samplesheet.py
+++ bin/fastq_dir_to_samplesheet.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# FASTQ DIR TO SAMPLESHEET BGI DATA VERSION
+# Muna Therapeutics
+# Laura Sans Comerma
+
+
 import os
 import sys
 import glob
@@ -90,54 +95,70 @@ def fastq_dir_to_samplesheet(
             )
         return sample
 
-    def get_fastqs(extension):
+    def get_fastqs(fastq_path, extension):
         """
         Needs to be sorted to ensure R1 and R2 are in the same order
         when merging technical replicates. Glob is not guaranteed to produce
         sorted results.
         See also https://stackoverflow.com/questions/6773584/how-is-pythons-glob-glob-ordered
         """
-        return sorted(glob.glob(os.path.join(fastq_dir, f"*{extension}"), recursive=False))
+
+        return sorted(glob.glob(os.path.join(fastq_path, f"*{extension}"), recursive=False))
 
     read_dict = {}
 
-    ## Get read 1 files
-    for read1_file in get_fastqs(read1_extension):
-        sample = sanitize_sample(read1_file, read1_extension)
-        if sample not in read_dict:
-            read_dict[sample] = {"R1": [], "R2": []}
-        read_dict[sample]["R1"].append(read1_file)
+    from os import walk
+    def get_subdirectories_bgi(fastq_dir):
+        f = []
+        for (dirpath, dirnames, filenames) in walk(fastq_dir):
+            f.extend(dirnames)
+            break
+        correct_path = [fastq_dir + x for x in f if isinstance(x, str)]
+        return correct_path
+    
+    fastq_dir_bgi = get_subdirectories_bgi(fastq_dir)
 
-    ## Get read 2 files
-    if not single_end:
-        for read2_file in get_fastqs(read2_extension):
-            sample = sanitize_sample(read2_file, read2_extension)
-            read_dict[sample]["R2"].append(read2_file)
+    for path in fastq_dir_bgi:
+        ## Get read 1 files
 
-    ## Write to file
-    if len(read_dict) > 0:
-        out_dir = os.path.dirname(samplesheet_file)
-        if out_dir and not os.path.exists(out_dir):
-            os.makedirs(out_dir)
+        for read1_file in get_fastqs(path, read1_extension):
+            sample = sanitize_sample(read1_file, read1_extension)
+            if sample not in read_dict:
+                read_dict[sample] = {"R1": [], "R2": []}
+            read_dict[sample]["R1"].append(read1_file)
+        
+        ## Get read 2 files
+        if not single_end:
+            for read2_file in get_fastqs(path, read2_extension):
+                sample = sanitize_sample(read2_file, read2_extension)
+                if sample not in read_dict:
+                    read_dict[sample] = {"R1": [], "R2": []}
+                read_dict[sample]["R2"].append(read2_file)
 
-        with open(samplesheet_file, "w") as fout:
-            header = ["sample", "fastq_1", "fastq_2", "strandedness"]
-            fout.write(",".join(header) + "\n")
-            for sample, reads in sorted(read_dict.items()):
-                for idx, read_1 in enumerate(reads["R1"]):
-                    read_2 = ""
-                    if idx < len(reads["R2"]):
-                        read_2 = reads["R2"][idx]
-                    sample_info = ",".join([sample, read_1, read_2, strandedness])
-                    fout.write(f"{sample_info}\n")
-    else:
-        error_str = "\nWARNING: No FastQ files found so samplesheet has not been created!\n\n"
-        error_str += "Please check the values provided for the:\n"
-        error_str += "  - Path to the directory containing the FastQ files\n"
-        error_str += "  - '--read1_extension' parameter\n"
-        error_str += "  - '--read2_extension' parameter\n"
-        print(error_str)
-        sys.exit(1)
+        ## Write to file
+        if len(read_dict) > 0:
+            out_dir = os.path.dirname(samplesheet_file)
+            if out_dir and not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+            with open(samplesheet_file, "w") as fout:
+                header = ["sample", "fastq_1", "fastq_2", "strandedness"]
+                fout.write(",".join(header) + "\n")
+                for sample, reads in sorted(read_dict.items()):
+                    for idx, read_1 in enumerate(reads["R1"]):
+                        read_2 = ""
+                        if idx < len(reads["R2"]):
+                            read_2 = reads["R2"][idx]
+                        sample_info = ",".join([sample, read_1, read_2, strandedness])
+                        fout.write(f"{sample_info}\n")
+        else:
+            error_str = "\nWARNING: No FastQ files found so samplesheet has not been created!\n\n"
+            error_str += "Please check the values provided for the:\n"
+            error_str += "  - Path to the directory containing the FastQ files\n"
+            error_str += "  - '--read1_extension' parameter\n"
+            error_str += "  - '--read2_extension' parameter\n"
+            print(error_str)
+            sys.exit(1)
 
 
 def main(args=None):
